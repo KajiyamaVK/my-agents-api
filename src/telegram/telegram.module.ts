2a84e8c96616d5ed9d2effa4b81ba8abc7a91059
KajiyamaVK <victor.kajiyama@gmail.com>
2026-01-24 12:43:57 -0300

    .

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TelegrafModule } from 'nestjs-telegraf';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TelegramService } from './telegram.service';
-import { PrismaModule } from '../prisma/prisma.module'; // Importar PrismaModule
+import { AiModule } from '../ai/ai.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { TokenModule } from '../llm/token/token.module';
 
 @Module({
   imports: [
-    PrismaModule, //
     TelegrafModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
@@ -14,6 +15,9 @@ import { PrismaModule } from '../prisma/prisma.module'; // Importar PrismaModule
       }),
       inject: [ConfigService],
     }),
+    AiModule,
+    PrismaModule,
+    TokenModule,
   ],
   providers: [TelegramService],
 })
